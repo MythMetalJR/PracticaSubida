3d579a3c4ee20064cd83af68067254c88a0585c8
Britanr <redrovanbritay1751623776gmail.com>
2025-06-11 00:25:21 -0500

    Creacion de la clase Britany

--- src/main/java/com/mycompany/practicasubida/ClaseBritany.java
+++ src/main/java/com/mycompany/practicasubida/ClaseBritany.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.practicasubida;
 
 /**
  *
- * @author User
+ * @author Ryzen 7
  */
-public class PracticaSubida {
-
-    public static void main(String[] args) {
-        System.out.println("soy britany");
-    }
+public class ClaseBritany {
+    
 }
